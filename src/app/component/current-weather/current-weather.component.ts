377961d599de87fb4146eabcc3fd8b0b54fad0a5
CristianCpi <54529569+CristianCpi@users.noreply.github.com>
2023-03-21 23:03:31 +0100

    Update current-weather.component.ts

--- src/app/component/current-weather/current-weather.component.ts
+++ src/app/component/current-weather/current-weather.component.ts
@@ -12,22 +12,7 @@ import { FetchWeatherService } from 'src/app/service/fetch-weather.service';
 })
 export class CurrentWeatherComponent implements OnInit {
   private destroy$ = new Subject<void>();
-
-  public weather: Weather = {
-    coord: {
-      lon: 0,
-      lat: 0,
-    },
-    main: {
-      temp: 0,
-      feels_like: 0,
-      temp_min: 0,
-      temp_max: 0,
-      pressure: 0,
-      humidity: 0,
-    },
-    name: '',
-  };
+  public weather = {} as Weather;
 
   private routeSub: Subscription | undefined;
   public lat = '';
